1ed81e6fea14c2d7c40dc257fa37337f98f7d711
kumagi <hiroki.kumazaki@gmail.com>
2018-08-23 17:29:44 +0900

    restart multiple times would recover only first recovery unit.

--- tests-core/src/foedus/restart/test_simple_bringup.cpp
+++ tests-core/src/foedus/restart/test_simple_bringup.cpp
@@ -23,12 +23,16 @@
 #include "foedus/engine_options.hpp"
 #include "foedus/test_common.hpp"
 #include "foedus/log/log_manager.hpp"
+#include "foedus/proc/proc_id.hpp"
+#include "foedus/proc/proc_manager.hpp"
 #include "foedus/snapshot/snapshot_manager.hpp"
 #include "foedus/storage/storage_manager.hpp"
 #include "foedus/storage/array/array_metadata.hpp"
 #include "foedus/storage/array/array_storage.hpp"
 #include "foedus/storage/masstree/masstree_metadata.hpp"
 #include "foedus/storage/masstree/masstree_storage.hpp"
+#include "foedus/thread/thread.hpp"
+#include "foedus/thread/thread_pool.hpp"
 #include "foedus/xct/xct_manager.hpp"
 
 
@@ -103,7 +107,75 @@ void test(bool wait_durable) {
 TEST(SimpleBringupTest, Durable) { test(true); }
 TEST(SimpleBringupTest, NonDurable) { test(false); }
 
+ErrorStack write_value(const proc::ProcArguments& args) {
+  thread::Thread* context = args.context_;
+  foedus::Engine* engine = args.engine_;
+  foedus::xct::XctManager* xct_manager = engine->get_xct_manager();
+  foedus::storage::masstree::MasstreeStorage masstree =
+    context->get_engine()->get_storage_manager()->get_masstree("my_masstree");
+  WRAP_ERROR_CODE(xct_manager->begin_xct(context, foedus::xct::kSerializable));
+  int cnt;
+  char value[30];
+  memcpy(&cnt, args.input_buffer_, sizeof(int));
+  std::string key = std::to_string(cnt);
+  memcpy(value, "value:", 6);
+  memcpy(value + 6, key.data(), key.size());
+  masstree.insert_record(context, &key[0], key.size(), value, key.size() + 6);
 
+  for (int i = 0; i < cnt; ++i) {
+    std::string key = std::to_string(i);
+    uint16_t payload_capacity = 40;
+    std::cout << "for idx: " << i << "\n";
+    foedus::ErrorCode result =  masstree.get_record(context, key.data(), key.size(), value, &payload_capacity, true);
+    EXPECT_NE(kErrorCodeStrKeyNotFound, result);
+    if (kErrorCodeStrKeyNotFound == result) {
+      foedus::ErrorStack err(result);
+      return err;
+    }
+    std::string val(value, payload_capacity);
+    std::cout << "val: " << val << "\n";
+    char expected[30];
+    memcpy(expected, "value:", 6);
+    memcpy(expected + 6, key.data(), key.size());
+    EXPECT_EQ(0, memcmp(expected, value, payload_capacity));
+  }
+  foedus::Epoch commit_epoch;
+
+  WRAP_ERROR_CODE(xct_manager->precommit_xct(context, &commit_epoch));
+  WRAP_ERROR_CODE(xct_manager->wait_for_commit(commit_epoch));
+  return kRetOk;
+}
+
+void test_restart() {
+  EngineOptions options = get_tiny_options();
+  options.log_.flush_at_shutdown_ = true;
+  options.debugging_.debug_log_min_threshold_ =
+    foedus::debugging::DebuggingOptions::kDebugLogError;
+  for (int i =0; i < 10; ++i) {
+    Engine engine(options);
+    engine.get_proc_manager()->pre_register("write_value", write_value);
+
+    COERCE_ERROR(engine.initialize());
+    {
+      UninitializeGuard guard(&engine);
+      storage::masstree::MasstreeMetadata meta("my_masstree");
+      if (!engine.get_storage_manager()->get_storage("my_masstree")->exists()) {
+        std::cout << "++++++++~~~~~~~~~~make the storage\n\n\n";
+        storage::masstree::MasstreeStorage db;
+        Epoch epoch;
+        COERCE_ERROR(engine.get_storage_manager()->create_masstree(&meta, &db, &epoch));
+      }
+
+      foedus::ErrorStack result =
+        engine.get_thread_pool()->impersonate_synchronous("write_value", &i, sizeof(int));
+      std::cout << "result[" << i << "]=" << result << std::endl;
+      COERCE_ERROR(engine.uninitialize());
+    }
+  }
+  cleanup_test(options);
+}
+
+TEST(SimpleBringupTest, RepeatBoot) { test_restart(); }
 }  // namespace restart
 }  // namespace foedus
 
